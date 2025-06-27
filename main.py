43b0877d078c4fab46c2a3d2fe54f047293e3288
Junyan Guo <jyguoeddie@gmail.com>
2025-06-27 21:49:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from modules.waiting_time_pede.waiting_time_pede import waiting_time_pede
 from modules.tracking_pede.tracking_pede import tracking_pede
 from modules.type_vehicle.type_vehicle import type_vehicle_analysis
 from modules.traffic_analysis.traffic_analysis import run_traffic_analysis
+from modules.age_gender.age_gender import run_age_gender_detection
 
 def main():
     parser = argparse.ArgumentParser(description="Pedestrian Analysis Toolbox")
@@ -11,8 +12,8 @@ def main():
         "--mode",
         type=str,
         required=True,
-        choices=["count", "waiting", "tracking", "type", "traffic"],  #
-        help="Choose the analysis mode: 'count', 'waiting', 'tracking', 'type', or 'traffic'",
+        choices=["count", "waiting", "tracking", "type", "traffic", "agegender"],
+        help="Choose the analysis mode: 'count', 'waiting', 'tracking', 'type', 'traffic', or 'agegender'",
     )
     parser.add_argument(
         "--zone_configuration_path",
@@ -115,6 +116,8 @@ def main():
             confidence_threshold=args.confidence_threshold,
             iou_threshold=args.iou_threshold,
         )
+    elif args.mode == "agegender":
+        run_age_gender_detection(args.source_video_path)
 
     else:
         print(f"Unknown mode: {args.mode}")

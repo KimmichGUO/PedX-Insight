d45bd593d637f106f16ac88baa7b83af5afda581
Junyan Guo <jyguoeddie@gmail.com>
2025-07-03 21:10:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from modules.weather.weather import run_weather_detection
 from modules.race.race import run_race_detection
 from modules.traffic_total.traffic_total import run_traffic_total_detection
 from modules.traffic_light.traffic_light import traffic_light
+from modules.traffic_sign.traffic_sign import traffic_sign_detection
+from modules.head.head import run_head_detection
+from modules.daynight.daytime import run_daytime_detection
 
 def main():
     parser = argparse.ArgumentParser(description="Pedestrian Analysis Toolbox")
@@ -16,7 +19,7 @@ def main():
         "--mode",
         type=str,
         required=True,
-        choices=["count", "waiting", "tracking", "type", "traffic", "agegender", "weather", "race", "total", "light"],
+        choices=["count", "waiting", "tracking", "type", "traffic", "agegender", "weather", "race", "total", "light", "head", "daytime"],
         help="Choose the analysis mode: 'count', 'waiting', 'tracking', 'type', 'traffic', or 'agegender'",
     )
     parser.add_argument(
@@ -141,7 +144,24 @@ def main():
         traffic_light(
             source_video_path=args.source_video_path,
         )
-
+    elif args.mode == "sign":
+        traffic_sign_detection(
+            source_video_path=args.source_video_path,
+            weights=args.source_weights_path,
+            confidence=args.confidence_threshold,
+            iou=args.iou_threshold,
+            device=args.device,
+            classes=args.classes,
+            target_video_path=args.target_video_path
+        )
+    elif args.mode == "head":
+        run_head_detection(
+            source_video_path=args.source_video_path
+        )
+    elif args.mode == "daytime":
+        run_daytime_detection(
+            source_video_path=args.source_video_path
+        )
     else:
         print(f"Unknown mode: {args.mode}")
 
